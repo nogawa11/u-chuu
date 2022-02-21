a2b32babd97c035b6c0f48b3f876f1dbf83741cb
David Yarhi <yarhi.david@gmail.com>
2022-02-21 14:52:40 +0900

    created seeds, added faker gem

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.3'
+# Faker gem https://github.com/faker-ruby/faker, used for seeds data
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.6'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Removing Old Planets..."
+Planet.destroy_all
+
+puts "Creating New Planets..."
+
+15.times do
+  Planet.create!(
+    name: Faker::Space.planet,
+    location: Faker::TvShows::StarTrek.location,
+    photos_url: "https://picsum.photos/200/300",
+    price: rand(1..999,999),
+    maximum_guests: rand(1..999,999,999),
+    description: Faker::Emotion.noun,
+    rotation_time: "#{rand(1..999)} days",
+    revolution_time: "#{rand(1..999)} days",
+    radius: "#{rand(1000..999,999)} Kilometers",
+    avg_temp: "#{rand(-500..500)} Celsius"
+  )
+end
+
+puts "#{Planet.count} Planets created."
