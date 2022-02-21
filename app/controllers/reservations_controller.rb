b96e4f277a8f341132f3dbc678c985ac480df7c1
David Yarhi <yarhi.david@gmail.com>
2022-02-21 16:41:12 +0900

    added controller for reviews and reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,39 @@
 class ReservationsController < ApplicationController
+  def new
+    @planet = Planet.find(params[:id])
+    @reservation = Reservation.new
+  end
+
+  def create
+    @planet = Planet.find(params[:id])
+    @user = @planet.user
+    @reservation = Reservation.new(reservation_params)
+    @reservation.planet = @planet
+    @reservation.user = @user
+
+    if @reservation.save
+      redirect_to reservations_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @reservation = Reservation.all
+  end
+
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    redirect_to reservations_path
+  end
+
+  def update
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:planet_id, :user_id, :status, :start_date, :end_date, :number_of_guests)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new
+    @user = User.find(params[:user_id])
+    @planet = Planet.find(params[:planet_id])
+    @review.user = @user
+    @review.planet = @planet
+
+    if @review.save
+      redirect_to planet_path(@planet)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_id, :planet_id, :content, :rating)
+  end
 end
