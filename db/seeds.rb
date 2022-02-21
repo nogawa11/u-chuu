2b0110e179d20e3fa29188bd88109c6add1f1fb0
Nicole Ogawa <nogawa11@gmail.com>
2022-02-21 16:51:14 +0900

    fixed cloudinary and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Removing Old Users..."
+Planet.destroy_all
 User.destroy_all
 
 puts "Creating New Users..."
