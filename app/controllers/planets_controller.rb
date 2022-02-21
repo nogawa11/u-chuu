eefe207b52fc7776ea63f026e63268f41bab4c9a
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-02-21 16:27:29 +0900

    added instance variables in planets show

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,2 +1,8 @@
 class PlanetsController < ApplicationController
+  def show
+    @planets = Planet.all
+    @planet = Planet.find(params[:id])
+    @reviews_avg = @planet.reviews.average(:rating)
+  end
+
 end
