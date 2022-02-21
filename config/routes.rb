09d10b5922721eeaa9ce7e216fbc4a99d84705ba
Nicole Ogawa <nogawa11@gmail.com>
2022-02-21 15:16:38 +0900

    added routes for reservations, reviews, and planets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :reservations
+  resources :planets do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
