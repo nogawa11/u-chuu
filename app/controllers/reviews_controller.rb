26789cc2ceeae0929ba3591c80478e8c224c2da0
Nicole Ogawa <nogawa11@gmail.com>
2022-02-21 19:42:04 +0900

    added pundits and validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,18 @@
 class ReviewsController < ApplicationController
   def new
     @planet = Planet.find(params[:planet_id])
-    @review = Review.new
+    # @review = Review.new
+    @review = current_user.reviews.new
+    authorize @review
   end
 
   def create
-    @review = Review.new(review_params)
-    @user = current_user
+    # @review = Review.new(review_params)
+    @review = current_user.reviews.new(review_params)
+    authorize @review
+    # @user = current_user
     @planet = Planet.find(params[:planet_id])
-    @review.user = @user
+    # @review.user = @user
     @review.planet = @planet
 
     if @review.save
