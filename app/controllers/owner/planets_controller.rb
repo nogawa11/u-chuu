70f82f7c9a0d578fe8c4308e8a4e791a75573c14
Nicole Ogawa <nogawa11@gmail.com>
2022-02-23 12:25:59 +0900

    fixed ordering for planets

--- app/controllers/owner/planets_controller.rb
+++ app/controllers/owner/planets_controller.rb
@@ -2,6 +2,6 @@ class Owner::PlanetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @planets = policy_scope(Planet).where(user: current_user)
+    @planets = policy_scope(Planet).where(user: current_user).order(name: :asc)
   end
 end
