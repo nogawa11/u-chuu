8ea506748990bdf9ff622462a7499beaae1078f4
Nicole Ogawa <nogawa11@gmail.com>
2022-02-25 19:07:50 +0900

    refactored planets controller

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,46 +1,34 @@
 class PlanetsController < ApplicationController
-
-  skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :set_planet, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_planet, only: %i[show edit update destroy]
 
   def index
     if params[:search].present?
-      @planets = policy_scope(Planet).where("name ILIKE ? OR description ILIKE ? OR location ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order(name: :asc)
+      sql_query = " \
+        name ILIKE :query \
+        OR description ILIKE :query \
+        OR location ILIKE :query \
+      "
+      @planets = policy_scope(Planet).where(sql_query, query: "%#{params[:search]}%").order(name: :asc)
     elsif params[:browse].present?
-      case params[:browse]
-      when "hot"
-        @planets = policy_scope(Planet).where("avg_temp > 100").order(name: :asc)
-      when "jungle"
-        @planets = policy_scope(Planet).where("description ILIKE ?", "%jungle%").order(name: :asc)
-      when "cold"
-        @planets = policy_scope(Planet).where("avg_temp < 0").order(name: :asc)
-      when "dark"
-        @planets = policy_scope(Planet).where("revolution_time > 500").order(name: :asc)
-      end
+      search(params[:browse])
     else
       @planets = policy_scope(Planet).order(name: :asc)
     end
   end
 
   def show
-    # @reservation = current_user.reservations.new
-    # authorize @reservation
     @reservation = Reservation.new
     @reviews_avg = @planet.reviews.any? ? (@planet.reviews.average(:rating)).round(1) : 0
     @review = Review.new
   end
 
   def new
-    # @planet = Planet.new
-    # @user = current_user
     @planet = current_user.planets.new
     authorize @planet
   end
 
   def create
-    # @planet = Planet.new(planet_params)
-    # @user = current_user
-    # @planet.user = @user
     @planet = current_user.planets.new(planet_params)
     authorize @planet
     if @planet.save
@@ -77,4 +65,16 @@ class PlanetsController < ApplicationController
     params.require(:planet).permit(:user_id, :name, :description, :price_per_night, :maximum_guests, :rotation_time, :revolution_time, :radius, :avg_temp, :location, photos: [])
   end
 
+  def search(params)
+    case params
+    when "hot"
+      @planets = policy_scope(Planet).where("avg_temp > 100").order(name: :asc)
+    when "jungle"
+      @planets = policy_scope(Planet).where("description ILIKE ?", "%jungle%").order(name: :asc)
+    when "cold"
+      @planets = policy_scope(Planet).where("avg_temp < 0").order(name: :asc)
+    when "dark"
+      @planets = policy_scope(Planet).where("revolution_time > 500").order(name: :asc)
+    end
+  end
 end
