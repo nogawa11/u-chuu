4b9a9c484c21ccd10b369ce08afc0b9627cfd94f
Amane Weston <amane.weston02@gmail.com>
2022-02-25 15:09:29 +0900

    set min value of price per night and max guests as one

--- db/schema.rb
+++ db/schema.rb
@@ -59,16 +59,6 @@ ActiveRecord::Schema.define(version: 2022_02_24_122753) do
     t.index ["user_id"], name: "index_planets_on_user_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.integer "rating"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
   create_table "reservations", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
@@ -119,7 +109,6 @@ ActiveRecord::Schema.define(version: 2022_02_24_122753) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "planets", "users"
-  add_foreign_key "profiles", "users"
   add_foreign_key "reservations", "planets"
   add_foreign_key "reservations", "users"
   add_foreign_key "reviews", "planets"
